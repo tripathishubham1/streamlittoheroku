232a53de854f441fa8def8ac7c367d852c9b493f
Shubham Tripathi <59553179+tripathishubham1@users.noreply.github.com>
2021-04-17 16:00:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ background-size: cover;
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Flipkart Rating Prediction 😢🙂😊 \n",)
-st.image('/content/—Pngtree—shopping on mobile_5354478.png', caption=None, width=200, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
+st.image('—Pngtree—shopping on mobile_5354478.png', caption=None, width=200, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 
 
 user_input = st.text_input("Input Review : ")
