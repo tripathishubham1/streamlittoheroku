1d2cc2bb424fb79bfaaae49cde8836edc0dd41ce
Shubham Tripathi <59553179+tripathishubham1@users.noreply.github.com>
2021-04-18 00:00:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Rating Predictor 😢🙂😊 \n",)
 st.image('—Pngtree—shopping on mobile_5354478.png', caption=None, width=200, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
-
+st.text('input text eg. your product is very good,worst product ever etc.')
 
 user_input = st.text_input("Input Review : ")
 k = preprocessing(user_input)
